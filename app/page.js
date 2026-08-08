69acf9844b12d40889e0fadda8f1bb7a87d86595
abhineshm <67950077+abhineshm@users.noreply.github.com>
2026-08-08 12:27:11 +0530

    Fix all import paths to use relative imports instead of @ alias

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Hero from '@/components/Hero'
-import Services from '@/components/Services'
-import Team from '@/components/Team'
-import Testimonials from '@/components/Testimonials'
-import Contact from '@/components/Contact'
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import Team from '../components/Team'
+import Testimonials from '../components/Testimonials'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
